c08f1185abc7818b8b2e974dec88ba2a20ef8b3f
Jameka Echols <jameka.echols@gmail.com>
2021-02-08 21:49:14 -0600

    changes additional buttons

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,18 +11,30 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    @IBOutlet weak var stopRecordButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordButton.isEnabled = false;
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called.")
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button pressed")
         recordingLabel.text = "Recording in Progress"
+        recordButton.isEnabled = false;
+        stopRecordButton.isEnabled = true;
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button pressed")
+        recordButton.isEnabled = true;
+        stopRecordButton.isEnabled = false;
+        recordingLabel.text = "Tap to Record"
+        
     }
 }
 
